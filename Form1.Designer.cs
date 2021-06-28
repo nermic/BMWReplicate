2fe42484d8a1c6725966d66d593220a1e3a7d26a
Thabo <thabo.who@gmail.com>
2021-06-28 09:09:21 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace BMWReplicate
+{
+    partial class frmReplicate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkIncludeSub = new System.Windows.Forms.CheckBox();
+            this.btnReplicate = new System.Windows.Forms.Button();
+            this.btnViewLog = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.btnBrowseDestination = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialogSource = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialogDestination = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // chkIncludeSub
+            // 
+            this.chkIncludeSub.AutoSize = true;
+            this.chkIncludeSub.Location = new System.Drawing.Point(134, 134);
+            this.chkIncludeSub.Name = "chkIncludeSub";
+            this.chkIncludeSub.Size = new System.Drawing.Size(136, 17);
+            this.chkIncludeSub.TabIndex = 0;
+            this.chkIncludeSub.Text = "Include Sub Directories";
+            this.chkIncludeSub.UseVisualStyleBackColor = true;
+            // 
+            // btnReplicate
+            // 
+            this.btnReplicate.Location = new System.Drawing.Point(134, 175);
+            this.btnReplicate.Name = "btnReplicate";
+            this.btnReplicate.Size = new System.Drawing.Size(75, 23);
+            this.btnReplicate.TabIndex = 1;
+            this.btnReplicate.Text = "&Replicate";
+            this.btnReplicate.UseVisualStyleBackColor = true;
+            this.btnReplicate.Click += new System.EventHandler(this.btnReplicate_Click);
+            // 
+            // btnViewLog
+            // 
+            this.btnViewLog.Location = new System.Drawing.Point(134, 280);
+            this.btnViewLog.Name = "btnViewLog";
+            this.btnViewLog.Size = new System.Drawing.Size(75, 23);
+            this.btnViewLog.TabIndex = 2;
+            this.btnViewLog.Text = "View &Log";
+            this.btnViewLog.UseVisualStyleBackColor = true;
+            this.btnViewLog.Click += new System.EventHandler(this.btnViewLog_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(655, 280);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(134, 233);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(596, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(134, 42);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(512, 20);
+            this.txtSource.TabIndex = 5;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(134, 86);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(512, 20);
+            this.txtDestination.TabIndex = 6;
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.Location = new System.Drawing.Point(655, 40);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSource.TabIndex = 7;
+            this.btnBrowseSource.Text = "Browse";
+            this.btnBrowseSource.UseVisualStyleBackColor = true;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // btnBrowseDestination
+            // 
+            this.btnBrowseDestination.Location = new System.Drawing.Point(655, 86);
+            this.btnBrowseDestination.Name = "btnBrowseDestination";
+            this.btnBrowseDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseDestination.TabIndex = 8;
+            this.btnBrowseDestination.Text = "Browse";
+            this.btnBrowseDestination.UseVisualStyleBackColor = true;
+            this.btnBrowseDestination.Click += new System.EventHandler(this.btnBrowseDestination_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Destination";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Progress Bar";
+            // 
+            // frmReplicate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 331);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowseDestination);
+            this.Controls.Add(this.btnBrowseSource);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnViewLog);
+            this.Controls.Add(this.btnReplicate);
+            this.Controls.Add(this.chkIncludeSub);
+            this.Name = "frmReplicate";
+            this.Text = "Replicate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkIncludeSub;
+        private System.Windows.Forms.Button btnReplicate;
+        private System.Windows.Forms.Button btnViewLog;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnBrowseSource;
+        private System.Windows.Forms.Button btnBrowseDestination;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogSource;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogDestination;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMWReplicate
+{
+    public partial class frmReplicate : Form
+    {
+        public frmReplicate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = folderBrowserDialogSource.ShowDialog(); 
+            if (result == DialogResult.OK) 
+            {
+                string file = folderBrowserDialogSource.SelectedPath;
+                try
+                {
+                    txtSource.Text = file;
+                }
+                catch (IOException)
+                {
+                }
+            }
+
+        }
+
+        private void btnBrowseDestination_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = folderBrowserDialogDestination.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = folderBrowserDialogDestination.SelectedPath;
+                try
+                {
+                    txtDestination.Text = file;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void btnReplicate_Click(object sender, EventArgs e)
+        {
+            // Invoke FindFiles() recursive function
+            if (string.IsNullOrEmpty(txtSource.Text))
+            {
+                MessageBox.Show("Please select a Source folder");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtDestination.Text))
+            {
+                MessageBox.Show("Please select a Destination folder");
+                return;
+            }
+            string SourcePath = txtSource.Text;
+            string DestinationPath = txtDestination.Text;
+
+            ReplicateFiles(SourcePath, DestinationPath, chkIncludeSub.Checked);
+        }
+
+        private bool ReplicateFiles(string SourcePath, string DestinationPath, bool includeSubs)
+        {
+            //// Loop thru each file in the current directory and print it's name
+            ////System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:\Thabo\BMW\logFile.txt"); //open the file for writing.
+            //foreach (string fileName in Directory.GetFiles(SourcePath))
+            //{
+            //    Console.WriteLine(fileName);
+            //    using (StreamWriter sw = File.AppendText(@"C:\Thabo\BMW\logFile.txt"))
+            //    {
+            //        sw.WriteLine(fileName);
+            //    }
+
+            //}
+
+            //// If there is a subdirectory in the current directory, then recursively 
+            //// call FindFiles() method. The recursion will break when the innermost 
+            //// directory with no subdirectory is found.
+            //if (includeSubs)
+            //{
+            //    foreach (string directory in Directory.GetDirectories(SourcePath))
+            //    {
+            //        // Notice that FindFiles() is calling itself
+            //        DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+            //        ReplicateFiles(directory, DestinationPath + directoryInfo.Name, includeSubs);
+            //    } 
+            //}
+
+            SourcePath = SourcePath.EndsWith(@"\") ? SourcePath : SourcePath + @"\";
+            DestinationPath = DestinationPath.EndsWith(@"\") ? DestinationPath : DestinationPath + @"\";
+
+            try
+            {
+                if (Directory.Exists(SourcePath))
+                {
+                    if (Directory.Exists(DestinationPath) == false)
+                    {
+                        Directory.CreateDirectory(DestinationPath);
+                    }
+
+                    progressBar1.Value = 1;
+                    progressBar1.Minimum = 1;
+                    progressBar1.Step = 1;
+                    progressBar1.Maximum = Directory.GetFiles(SourcePath).Length;
+
+
+                    foreach (string files in Directory.GetFiles(SourcePath))
+                    {
+                       
+                        FileInfo fileInfo = new FileInfo(files);
+                        if (File.Exists(DestinationPath + fileInfo.Name))
+                        {
+                            FileInfo fileInfo2 = new FileInfo(DestinationPath + fileInfo.Name);
+                            if (fileInfo.LastWriteTime != fileInfo2.LastWriteTime)
+                            {
+                                fileInfo.CopyTo(string.Format(@"{0}\{1}", DestinationPath, fileInfo.Name), true);
+                                progressBar1.PerformStep();
+                                using (StreamWriter sw = File.AppendText(@"C:\Thabo\BMW\logFile.txt"))
+                                {
+                                    sw.WriteLine(string.Format(@"Copy file from  {0} to {1}", SourcePath + fileInfo.Name, DestinationPath + fileInfo.Name));
+                                }
+                            }
+                           
+                        }
+                        else
+                        {
+                            fileInfo.CopyTo(string.Format(@"{0}\{1}", DestinationPath, fileInfo.Name), true);
+                            progressBar1.PerformStep();
+                            using (StreamWriter sw = File.AppendText(@"C:\Thabo\BMW\logFile.txt"))
+                            {
+                                sw.WriteLine(string.Format(@"Copy file from  {0} to {1}", SourcePath + fileInfo.Name, DestinationPath + fileInfo.Name));
+                            }
+                        }
+                       
+                    }
+
+                    foreach (string drs in Directory.GetDirectories(SourcePath))
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(drs);
+                        if (ReplicateFiles(drs, DestinationPath + directoryInfo.Name, includeSubs) == false)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        private void btnViewLog_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Thabo\BMW\logFile.txt");
+        }
+    }
+}
